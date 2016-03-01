ff50e49b6888ec8ffea603622dbdbae202e7c3df
Argha K Roy <argha@373k.net>
2016-03-02 01:53:25 +0600

    flavour files

---
+++ app/src/master/java/com/polluxlab/utils/ReleaseDetails.java
@@ -0,0 +1,11 @@
+package com.polluxlab.utils;
+
+/**
+ * Created by ARGHA K ROY on 7/1/2015.
+ */
+public class ReleaseDetails {
+    public enum Type{
+        MASTER,DEVELOPER
+    }
+    public static final Type type=Type.MASTER;
+}
