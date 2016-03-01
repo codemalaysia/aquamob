db71a38b26809ce9120d30fa66f5bd368e26003f
Argha K Roy <argha@373k.net>
2016-03-02 01:45:37 +0600

    added app constants

---
+++ app/src/main/java/com/polluxlab/aquamob/utils/AppConst.java
@@ -0,0 +1,24 @@
+package com.polluxlab.aquamob.utils;
+
+/**
+ * All cross class used constants are declared here
+ * Created by ARGHA K ROY on 5/7/2015.
+ */
+public class AppConst {
+    public static String APP_PREF="aquamob_pref";
+    public static String PREF_USERNAME="pref_username";
+    public static String PREF_FORM_IDS="pref_form_ids";
+    public static String PREF_USER_INFOS="pref_user_infos";
+
+    public static String DEBUG_KEY="Lobster";
+    public static int MAX_CONNECTION_TRY = 1;
+    public static int TIME_OUT = 20000;//in millisecond
+
+    public static final String DB_NAME="AQUAMOB_DB.db";
+    public static final String DB_TABLE_DOWNLOADED_FORMS="downloadedForms";
+    public static final String DB_TABLE_SAVED_FORMS="savedForms";
+    public static final int DB_VERSION=1;
+
+    public static final String NEW_ACC_LINK="http://goo.gl/forms/XWGMWlKvjp";
+    public static final String FORGOT_USERNAME_LINK="http://goo.gl/forms/GORSdnMr9d"; // http://www.aquamob.com/ > Free Demo . Google form for new username
+}
