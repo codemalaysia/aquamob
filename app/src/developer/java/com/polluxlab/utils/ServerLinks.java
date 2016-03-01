c3c8b3a1726e45bd9133619eca732e84c2383517
Argha K Roy <argha@373k.net>
2016-03-02 01:55:52 +0600

    flavour files

---
+++ app/src/developer/java/com/polluxlab/utils/ServerLinks.java
@@ -0,0 +1,9 @@
+package com.polluxlab.utils;
+
+/**
+ * Created by ARGHA K ROY on 8/5/2015.
+ */
+public class ServerLinks {
+    public static String ENDPOINT = "http://128.199.216.163/lobsterv2/dev/web/app_dev.php/api";
+    public static String SEND_DATA_URL = "http://128.199.216.163/lobsterv2/dev/web/app_dev.php/api/form/%s/form-data";
+}
---
+++ app/src/master/java/com/polluxlab/utils/ServerLinks.java
@@ -0,0 +1,9 @@
+package com.polluxlab.utils;
+
+/**
+ * Created by ARGHA K ROY on 8/5/2015.
+ */
+public class ServerLinks {
+    public static String ENDPOINT = "http://128.199.161.102/aquamob-prod/web/app.php/api";
+    public static String SEND_DATA_URL = "http://128.199.161.102/aquamob-prod/web/app.php/api/form/%s/form-data";
+}
