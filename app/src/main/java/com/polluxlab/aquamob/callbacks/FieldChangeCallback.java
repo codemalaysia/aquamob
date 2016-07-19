5b28dc60e4d6824d06690f9aedfc6df9de69200a
ARGHA <argha55@gmail.com>
2016-07-20 01:15:03 +0600

    added new callbacks

---
+++ app/src/main/java/com/polluxlab/aquamob/callbacks/FieldChangeCallback.java
@@ -0,0 +1,12 @@
+package com.polluxlab.aquamob.callbacks;
+
+import android.view.View;
+
+/**
+ * Author ARGHA_K_ROY
+ * Date 5/27/2016.
+ */
+public interface FieldChangeCallback {
+
+    void onFieldChange(View view);
+}
